675d87a69d39d018549c213715a2946f3999b313
Wesam Jabali <wesam@mini.local>
2021-02-18 22:51:22 -0600

    Fixed nav

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,14 @@
 <template>
-  <div v-if="isLoggedIn">
+  <div>
     <v-app-bar app color="#6A76AB" dark>
     <v-app-bar-nav-icon
+      v-if="isLoggedIn"
       @click="drawer = !drawer"
     ></v-app-bar-nav-icon>
     <v-toolbar-title class="white--text">D3L</v-toolbar-title>
     <v-spacer></v-spacer>
-        <v-btn text>
+      <div v-if="isLoggedIn">
+        <v-btn v-if="isFaculty" text>
           Faculty Support
         </v-btn>
         <v-btn text class="ml-2">
@@ -21,6 +23,7 @@
           </v-icon>
           {{ this.$store.getters.first_name + ' ' + this.$store.getters.last_name }}
         </v-btn>
+      </div>
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer">
@@ -29,16 +32,16 @@
       </div>
       <v-divider></v-divider>
       <v-list nav dense>
-        <v-list-item-group active-class="text--accent-4">
+        <v-list-item-group active-class="text--accent-4" v-if="isLoggedIn">
 
-          <v-list-item v-if="isLoggedIn" to="/home">
+          <v-list-item to="/home">
             <v-list-item-icon>
               <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Punch Clock</v-list-item-title>
           </v-list-item>
 
-          <v-list-item v-if="isLoggedIn && isAdmin" to="/addRole">
+          <v-list-item v-if="isAdmin" to="/addRole">
             <v-list-item-icon>
               <v-icon>mdi-account</v-icon>
             </v-list-item-icon>
@@ -61,11 +64,7 @@
         </v-btn>
       </div>
       
-    </v-navigation-drawer>
-   
-    <v-container style="height: 120px;"></v-container>
-   
-    
+    </v-navigation-drawer>    
   </div>
   
 </template>
@@ -86,9 +85,6 @@ export default {
     },
     isFaculty() {
       return this.$store.getters.roles.includes('faculty');
-    },
-    email() {
-      return this.$store.getters.email;
     },
   },
 };
