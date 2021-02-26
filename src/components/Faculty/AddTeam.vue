85bca53a58c180168b7b30cd6f5621340ce5e8c7
Wesam Jabali <wesamjabali@icloud.com>
2021-02-26 15:13:30 -0600

    Fix mobile file upload

--- src/components/Faculty/AddTeam.vue
+++ src/components/Faculty/AddTeam.vue
@@ -62,6 +62,11 @@ export default {
   mounted() {
     this.get_all_users();
     this.get_all_teams();
+    // fix for v-file-input not triggering properly
+    let inputs = document.querySelectorAll(".v-file-input input");
+    [...inputs].forEach((input) => {
+      input.remove();
+    });
   },
   methods: {
     async submit() {
