ff52bdc73e99677842ed7bb6a36b4f79062027c1
Wesam Jabali <wesamjabali@icloud.com>
2021-03-01 16:59:10 -0600

    Make delete account prettier

--- src/components/User/DeleteAccount.vue
+++ src/components/User/DeleteAccount.vue
@@ -1,18 +1,16 @@
 <template>
   <v-dialog width="500px" v-model="dialog">
     <v-card>
-      <div class="pa-4 secondary white--text">
-        <div class="title">
-          Are you sure you want to delete your account?
-        </div>
-        <div class="caption">
+      <div class="pa-4 title secondary white--text text-center">
+          Delete account
+      </div>
+        <div class="pt-5 text-center">
           This action cannot be undone!
         </div>
-      </div>
-      <v-card-actions class="py-4">
+      <v-card-actions class="pt-4">
         <v-spacer />
-        <v-btn color="error" outlined v-on:click="submit"> Delete Account </v-btn>
-        <v-btn color="success" v-on:click="$emit('done')"> Cancel </v-btn>
+        <v-btn color="error" text v-on:click="submit"> Delete Account </v-btn>
+        <v-btn text v-on:click="$emit('done')"> Cancel </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
